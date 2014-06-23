bae9f6d8f3d197a9074cc86d78b313dbbd9ca055
pk1a <pkozlowski@amadeus.com>
2014-06-23 13:27:46 +0200

    fix: properly report transpilation errors
    Fixes #1

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ module.exports = function () {
 
         if (file.isBuffer()) {
             try {
-                var transpileResult = hspTranspiler(String(file.contents), file.path);
+                var contentAsString = String(file.contents);
+                var transpileResult = {changed: false};
+
+                try {
+                    transpileResult = hspTranspiler(contentAsString, file.path);
+                } catch (e) {
+                    this.emit('error', new PluginError('gulp-hsp',
+                        'Transpilation error in "' + file.path + '" at '+ e.line + ':' + e.col + ': ' + e.message), {
+                        fileName: file.path,
+                        lineNumber: e.line,
+                        stack: e.stack
+                    });
+                }
+
                 if (transpileResult.changed) {
                     file.contents = new Buffer(transpileResult.code);
                 }
